7489dfe2bd1469e9570b28c29c64817bbd4fd591
OtabekAbdurakhmonov <77769473+OtabekAbdurakhmonov@users.noreply.github.com>
2022-01-24 17:37:12 +0900

    done

---
+++ src/data.jsx
@@ -0,0 +1,25 @@
+const reviews = [
+  {
+    id: 1,
+    name: 'John Smith',
+    job: 'Frontend Developer',
+    image: 'https://www.kindpng.com/picc/m/160-1600378_transparent-happy-person-png-happy-man-face-png.png',
+    text: 'I am a Frontend Web Developer who is comfortable working with JavaScript and React. Also, I can work with Material-UI and other libraries'
+  },
+  {
+    id: 2,
+    name: 'Anna Smith',
+    job: 'Backend Developer',
+    image: 'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmFuZG9tJTIwcGVyc29ufGVufDB8fDB8fA%3D%3D&w=1000&q=80',
+    text: 'I am a Backend Web Developer who is comfortable working with Node js and Python. Also, I can work with Django and other libraries'
+  },
+  {
+    id: 3,
+    name: 'David Smith',
+    job: 'Full Stack Developer',
+    image: 'https://www.incimages.com/uploaded_files/image/1920x1080/getty_481292845_77896.jpg',
+    text: 'I am a Full Stack Web Developer who is comfortable working with JavaScript, React, Node js, Django, GraphQL, Typescript and Next js. Also, I can work with Rest API and other libraries'
+  },
+];
+
+export default reviews;
